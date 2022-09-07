e9ebca5f41adb47e4e651e8797a722d8997a92b8
Everton P <evertonportela@outlook.com>
2022-09-07 00:22:47 -0300

    add aula 02
    anotacoes da segunda aula

---
+++ Practice 02 - tipos de dados/practice02.js
@@ -0,0 +1,33 @@
+/**
+ * Conceitos e escrita
+ * tipos de dados mais utilizados
+ */
+
+
+/* String com Aspas Duplas*/
+console.log("Everton")
+
+/* String com Aspas Simples*/
+console.log('Everton')
+
+/* String com Crases permitem quebra de linha*/
+console.log(`Texto com 
+"quebra" 
+de linhas`)
+
+/*Crases também permitem inserir expressoes*/
+console.log(`Everton ${1+1}`)
+
+/**
+ * Numbers
+ * 33 // inteiros
+*/
+console.log(33)
+/* 12.5 // reais - float */
+console.log(33/3.5)
+/* NaN // Not a Number */
+console.log(33 / "asdf")
+/* Infinity // infinito */
+console.log(12.5 === Infinity)
+console.log(Infinity)
+
